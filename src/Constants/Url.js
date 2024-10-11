96345d97034e4ddd2918dd69b3f4d72c8a54f2ec
Nakami <natalia.rivas@correo.unimet.edu.ve>
2024-10-11 01:01:15 -0400

    fix: homepage

--- src/Constants/Url.js
+++ src/Constants/Url.js
@@ -1,4 +1,4 @@
-export const HOME_URL = "/";
+export const HOME_URL = "/CryptoNavigate";
 export const Login_URL = "/login"
 export const Register_URL = "/register"
 export const AllCrypto_URL="/allCrytocurrencies"
